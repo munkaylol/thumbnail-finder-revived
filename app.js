ec486df279cc069d8797570853ad55a7236b4358
Jack <70031837+munkaylol@users.noreply.github.com>
2024-06-21 11:08:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,74 +1,22 @@
 document.getElementById("noScriptHide").style.display = "";
 
 function getAll() {
-    var subdomains = [
-        "i",
-        "i1",
-        "i2",
-        "i3",
-        "i4",
-        "img"
-    ];
-    var subdowmainsi_vi_webp = [
-        "i-vi_webp"
-    ];
-    var subdowmainsi1_vi_webp = [
-        "i1-vi_webp"
-    ];
-    var subdowmainsi2_vi_webp = [
-        "i2-vi_webp"
-    ];
-    var subdowmainsi3_vi_webp = [
-        "i3-vi_webp"
-    ];
-    var subdowmainsi4_vi_webp = [
-        "i4-vi_webp"
-    ];
-    var subdowmainsimg_vi_webp = [
-        "img-vi_webp"
-    ]
-    var subdowmainsimg_youtube = [
-        "img-youtube"
-    ]
-    var subdowmainsimg_youtube_vi_webp = [
-        "img-youtube-vi_webp"
-    ]
-    
-    for (var a in subdomains) {
-        document.getElementById(subdomains[a]).innerHTML = "Testing..."
-        getThumbnail(subdomains[a])
-    }
-    for (var a in subdowmainsi_vi_webp) {
-        document.getElementById(subdowmainsi_vi_webp[a]).innerHTML = "Testing..."
-        getiViWebpThumbnail(subdowmainsi_vi_webp[a])
-    }
-    for (var a in subdowmainsi1_vi_webp) {
-        document.getElementById(subdowmainsi1_vi_webp[a]).innerHTML = "Testing..."
-        geti1ViWebpThumbnail(subdowmainsi1_vi_webp[a])
-    }
-    for (var a in subdowmainsi2_vi_webp) {
-        document.getElementById(subdowmainsi2_vi_webp[a]).innerHTML = "Testing..."
-        geti2ViWebpThumbnail(subdowmainsi2_vi_webp[a])
-    }
-    for (var a in subdowmainsi3_vi_webp) {
-        document.getElementById(subdowmainsi3_vi_webp[a]).innerHTML = "Testing..."
-        geti3ViWebpThumbnail(subdowmainsi3_vi_webp[a])
-    }
-    for (var a in subdowmainsi4_vi_webp) {
-        document.getElementById(subdowmainsi4_vi_webp[a]).innerHTML = "Testing..."
-        geti4ViWebpThumbnail(subdowmainsi4_vi_webp[a])
-    }
-    for (var a in subdowmainsimg_vi_webp) {
-        document.getElementById(subdowmainsimg_vi_webp[a]).innerHTML = "Testing..."
-        getimgViWebpThumbnail(subdowmainsimg_vi_webp[a])
-    }
-    for (var a in subdowmainsimg_youtube) {
-        document.getElementById(subdowmainsimg_youtube[a]).innerHTML = "Testing..."
-        getimgyoutubeThumbnail(subdowmainsimg_youtube[a])
-    }
-    for (var a in subdowmainsimg_youtube_vi_webp) {
-        document.getElementById(subdowmainsimg_youtube_vi_webp[a]).innerHTML = "Testing..."
-        getimgyoutubeViWebpThumbnail(subdowmainsimg_youtube_vi_webp[a])
+    const subdomains = new Map([
+        [['i', "i1", "i2", "i3", "i4", "img"], getThumbnail],
+        [["i-vi_webp"], getiViWebpThumbnail],
+        [["i1-vi_webp"], geti1ViWebpThumbnail],
+        [["i2-vi_webp"], geti2ViWebpThumbnail],
+        [["i3-vi_webp"], geti3ViWebpThumbnail],
+        [["i4-vi_webp"], geti4ViWebpThumbnail],
+        [["img-vi_webp"], getimgViWebpThumbnail],
+        [["img-youtube"], getimgyoutubeThumbnail],
+        [["img-youtube-vi_webp"], getimgyoutubeViWebpThumbnail]
+    ]);
+
+    for (const [subarr, func] of subdomains) {
+        subarr.forEach(id => document.getElementById(id).innerHTML = "Testing...");
+
+        subarr.forEach(func);
     }
 }
 
